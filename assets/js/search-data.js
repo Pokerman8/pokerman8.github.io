8e5c6caae6a2114032ad6951af7c333e5640f1ab
Pokerman8 <1633100456@qq.com>
2025-11-15 05:05:21 +0000

    Deploying to gh-pages from @ Pokerman8/pokerman8.github.io@bc354e52c84947b3b9a6dd5e6f474c4d1abba6b9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-after-a-long-time-of-struggling-i-finally-decided-to-create-my-personal-website",
+          title: 'After a long time of struggling, I finally decided to create my personal...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
