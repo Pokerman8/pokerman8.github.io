6fc3b12c0c97aadc9aa7e47770aa68788be493c7
Pokerman8 <1633100456@qq.com>
2025-11-19 03:24:25 +0000

    Deploying to gh-pages from @ Pokerman8/pokerman8.github.io@465acb314a3f03dd413f798cf2da8b9a3eba215c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -104,14 +104,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-new-project-skel-cf-a-coarse-to-fine-biomechanical-skeleton-and-surface-mesh-recovery-framework",
-          title: 'New project SKEL-CF: a coarse-to-fine biomechanical skeleton and surface mesh recovery framework.',
-          description: "",
-          section: "News",},{id: "news-a-paper-accepted-by-the-visual-computer-international-journal-of-computer-graphics-2025",
+            },},{id: "news-a-paper-accepted-by-the-visual-computer-international-journal-of-computer-graphics-2025",
           title: 'A paper accepted by The Visual Computer International Journal of Computer Graphics 2025...',
           description: "",
           section: "News",},{id: "news-sparkles-smile-after-a-long-time-of-struggling-i-finally-decided-to-create-my-personal-website",
           title: ':sparkles: :smile: After a long time of struggling, I finally decided to create...',
+          description: "",
+          section: "News",},{id: "news-new-project-skel-cf-a-biomechanical-skeleton-and-surface-mesh-recovery-framework",
+          title: 'New project SKEL-CF: a biomechanical skeleton and surface mesh recovery framework.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
